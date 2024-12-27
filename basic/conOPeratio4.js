05d451644cc17610a81f61e8f58202857d9f65e4
Yash Pal Singh <ypal71786@gmail.com>
2024-12-27 12:44:13 +0530

    conversion Update

--- basic/conOPeratio4.js
+++ basic/conOPeratio4.js
@@ -1,9 +1,9 @@
 let score = "YASH";
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33"=>33
@@ -14,7 +14,7 @@ console.log(valueInNumber)
 let isLoggedIn = "Yash"
 
 let booleanIsLoggesdIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggesdIn)
+// console.log(booleanIsLoggesdIn)
 
 //0=>false
 //1=true
@@ -24,5 +24,35 @@ console.log(booleanIsLoggesdIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// operation
+// let value =3
+// let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2); 
+// console.log(2**2);
+// console.log(2%3);
+
+// let str1="hello"
+// let str2=" world"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+1);
+// console.log(1+"1");
+// console.log("1"+1+2);
+// console.log(1+2+"2");
+// console.log(3+(4*5)%3);
+
+
+// console.log(+true);
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
