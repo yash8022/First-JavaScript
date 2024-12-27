9cb46c0d13128b0285b12ba9e72cf78e609d76a0
Yash Pal Singh <ypal71786@gmail.com>
2024-12-27 11:40:41 +0530

    DatabaseLearn

---
+++ basic/Database3.js
@@ -0,0 +1,22 @@
+"use strict";//treat all Js code as newer version
+// alert ( 3 + 3 ); this will give error
+//we are using node.js not browser
+console.log(3+3)
+console.log("Yash")
+
+let name="hitesh"
+let age=20
+let isLoggedIn=true
+let state;
+
+//number=>2 to power 53
+//bigint=>2 to power 53 to 2 to power 64
+//string=""
+//boolean=true/false
+// undefined=>no value
+//null=>no 
+
+console.log(typeof age);
+console.log(typeof "Yash");
+console.log(typeof null);
+console.log(typeof undefined);
