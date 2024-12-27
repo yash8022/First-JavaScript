b27f6f2aca2c1acf2ce9c5d8279b5f20c92fb5ba
Yash Pal Singh <ypal71786@gmail.com>
2024-12-27 10:25:22 +0530

    variables learned

---
+++ basic/variables2.js
@@ -0,0 +1,17 @@
+const accountId=1123
+let accountEmail ="ypal71786@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2 not allowed
+ 
+accountEmail="yashhdh@gmail.com"
+accountPassword="123"
+accountCity="Delhi" 
+/*
+Perfer not to use var
+because of issue in block space anf function scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
