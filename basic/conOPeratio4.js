7a88171997dd95ba207138fc2bd51183b37275f3
Yash Pal Singh <ypal71786@gmail.com>
2024-12-27 11:40:59 +0530

    conversion

---
+++ basic/conOPeratio4.js
@@ -0,0 +1,28 @@
+let score = "YASH";
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33"=>33
+// "33abc"=>NaN
+// true=1;
+// false=0
+
+let isLoggedIn = "Yash"
+
+let booleanIsLoggesdIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggesdIn)
+
+//0=>false
+//1=true
+//""=false
+//"Yash"=true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
