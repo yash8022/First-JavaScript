f8d7e8ac9b154d5b84c10f5c29e7779ef56100f4
Yash Pal Singh <ypal71786@gmail.com>
2024-12-27 13:39:04 +0530

    data in detail

---
+++ basic/DTSsum6.js
@@ -0,0 +1,37 @@
+//Data  types of two types only 
+//Permitive--
+//7 types: String,Number,Boolean,null,undefined,BigInt,Symbol.
+
+ const score = 100
+ const scoreValue=100.3
+
+ const isLoddedIn= false
+ const outsideTemp=null;
+ let userEmail=undefined;
+
+ const id=Symbol('123')
+ const anotherId=Symbol('123') 
+
+ console.log(id===anotherId);
+
+ const bigNumber=232253538483532324425n
+//js is dynamically Typed language assigned at the time of
+//compilation or code exection
+
+
+
+//Reference(Non Pimitive)--
+//3 types: Array,Objects,Functions.
+
+const heroos=["yash","ash","Khushi"]
+
+let myobj={
+   name:"Yash",
+   age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello Wold");
+}
+
+console.log(typeof myFunction);
