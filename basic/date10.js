aeca52782b7b47384d6f4c1559aa6b8618633772
Yash Pal Singh <ypal71786@gmail.com>
2024-12-30 10:07:51 +0530

    Array BasIC

---
+++ basic/date10.js
@@ -0,0 +1,23 @@
+//dates
+
+let myDate =new Date()
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate=new Date(2023,0,23,5,3)
+let myCreatedDate=new Date("12-30-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
